519de3ccdc77a97cb6bfc2a35b6ad949172d4a48
Joe Endris <joseph.endris@outlook.com>
2023-09-20 19:52:35 -0500

    update after table 3

--- exercise_3.R
+++ exercise_3.R
@@ -11,49 +11,27 @@ library(flextable)
 library(lubridate)
 library(scales)
 
-seedlings <- read_excel("data/seedlings.xlsx")
+LT50 <- read_excel("data/LT50_data.xlsx")
 
+#omit NA in before/after column 
+LT50 <- LT50[complete.cases(LT50[,8]),]
 
-#create column with the mean height
-seedlings <- seedlings %>% 
-  group_by(species) %>%
-  mutate(mean_height = mean(height))
-
-#create dataframe for SD  
-sd_seedlings <- seedlings %>%
-  group_by(species) %>%
-  summarize(mean_height = mean(height), 
-            seedling_sd=sd(height))
-
-table3 <- seedlings %>%
-  select(species, height, mean_dia) %>%
-  tbl_summary(by = species,
-              missing= "no",
-              digits = all_continuous() ~1,
-              label = list(mean_dia ~ "Mean diameter (cm)",
-                           height ~ "height (cm)"),
-              statistic = list(all_continuous() ~ "{mean} ({sd})",
-                               all_categorical() ~ "{n}")) %>%
-  add_p(pvalue_fun = ~ style_pvalue(.x, digits = 3)) %>% #number of digits displayed for p-values
-  modify_caption("Table 2. Mean stem diameter and overall height measurements (cm) of three tree seedling species.") %>%
-  modify_footnote(everything() ~ NA) %>%
-  modify_header(
-    update = list(
-      label ~ '',
-      stat_1 ~ '***Acer saccharum***', #is markdown **bold** formatting
-      stat_2 ~ '***Quercus alba***',
-      stat_3 ~ '***Quercus coccinea***',
-      p.value ~ '**P-value**')) %>%
-  as_gt() %>%
-  gt::tab_options(heading.align = "left")
-table3
+#create dataframe for SD
+LT50_sd <- LT50%>%
+  group_by(state, species) %>%
+  dplyr::summarise(meanLT50=mean(LT50),
+                   sdLT50=sd(LT50))
 
 
 
-table2_plot <- ggplot(data= seedlings, aes(x=species, y= height, group = species))+
-  geom_boxplot()+
-  geom_errorbar(aes(x= species, ymin= mean_height-sd_seedlings$seedling_sd, ymax=mean_height+sd_seedlings$seedling_sd, group=species, width= 0.5))+
-  ylab("Height (cm)")+
+
+
+
+table3_plot <- ggplot(data= LT50_sd, aes(x=state, y= meanLT50, group = species, color = species))+
+  geom_violin(trim = FALSE, position = position_dodge(width = 1))+
+  geom_errorbar(aes(x= state, ymin= meanLT50-LT50_sd$sdLT50, ymax=meanLT50+LT50_sd$sdLT50, group=species, width= 0.5), position = position_dodge(width = 1))+
+  scale_color_manual(values = c("Acer saccharum" = "red", "Liriodendron tulipifera" = "blue", "Fagus grandifolia" = "black"))+
+  labs(y=expression("LT"["50"]/"Temperature (°C)"))+
   xlab("Species")+
   theme_bw()+
   theme(panel.border = element_blank(),  
@@ -62,6 +40,6 @@ table2_plot <- ggplot(data= seedlings, aes(x=species, y= height, group = species
         panel.background = element_blank(),
         axis.line = element_line(colour = "black"),
         axis.title.x = element_blank())+
-  ggtitle("Seedling height by species")
+  ggtitle("LT50 by state and species")
 
-table2_plot
+table3_plot
